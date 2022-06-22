6496018ee3c292086b33286b97797200dd3c9596
swaroop <swaroopaee98@gmail.com>
2022-06-22 21:29:08 +0530

    New project in old repo -Airbnb-clone

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ const App=()=>{
   return (
     <div className="App">
       <div className="App-header">
-        <h1>hello world</h1>
+        <h1>Starting New project -Airbnb clone</h1>
+        <p>date:22nd June 2022</p>
       </div>
     </div>
   );
